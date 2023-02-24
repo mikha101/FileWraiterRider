e389ed53efe3f940bd590206e4fc22fc7a65e848
«Elena <helenvit599@gmail.com>
2023-02-24 14:02:52 +0500

    Finished local File Storage 2

--- app/src/main/java/ru/synergy/filewraiterrider/MainActivity.java
+++ app/src/main/java/ru/synergy/filewraiterrider/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         FileOutputStream fos = null;
 
         try {
-        EditText textBox = (EditText) view.findViewById(R.id.editor);
+        EditText textBox = (EditText) findViewById(R.id.editor);
         String text = textBox.getText().toString();
 
         fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
     // Открытие файла
     public void openText(View view){
         FileInputStream fin = null;
-        TextView textView = (TextView) view.findViewById(R.id.text);
+        TextView textView = (TextView) findViewById(R.id.text);
 
         try {
             fin = openFileInput(FILE_NAME);
